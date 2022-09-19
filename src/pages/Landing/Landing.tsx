050cd755f0361f47c1291e22d7551d092eb324a9
cbadour <cbadour@addbloom.com>
2022-09-19 10:34:28 +0300

    fix landing email field

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import PrimaryButton from '../../components/buttons/PrimaryButton/PrimaryButton';
 import GozoIcon from '../../components/icons/GozoIcon/GozoIcon';
 import PrimaryCheckbox from '../../components/inputs/PrimaryCheckbox/PrimaryCheckbox';
+import PrimaryInput from '../../components/inputs/PrimaryInput/PrimaryInput';
 import BottomFixedContainer from '../../components/layout/BottomFixedContainer/BottomFixedContainer';
 import PrimaryContainer from '../../components/layout/PrimaryContainer/PrimaryContainer';
 import PrimaryTypography from '../../components/typography/PrimaryTypography/PrimaryTypography';
@@ -54,14 +55,19 @@ const Landing: React.FC = () => {
                     {
                         !isAuthenticated ?
                             <>
+                                <PrimaryInput
+                                    placeholder='Enter your email'
+                                    value={email}
+                                    onChange={setEmail} />
                                 {authError && <PrimaryTypography
                                     color='danger'
                                     customClassName={styles.error}>
                                     {authError}
                                 </PrimaryTypography>}
+                                <br />
                                 <TermsAcceptance />
                                 <PrimaryButton
-                                    onClick={login}
+                                    onClick={() => login(email)}
                                     expand='block'
                                     disabled={isAuthenticating || !accepted}>
                                     get started

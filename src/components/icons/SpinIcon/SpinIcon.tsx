7429271f7cebc7625be04771d154df93eebb6fc9
Rawad Nasr <rnasr@addbloom.com>
2022-10-10 10:06:26 +0300

    more customization to spin icon

--- src/components/icons/SpinIcon/SpinIcon.tsx
+++ src/components/icons/SpinIcon/SpinIcon.tsx
@@ -2,12 +2,13 @@ import { IonIcon } from "@ionic/react"
 
 interface Props {
     color?: string,
-    size?: string
+    size?: string,
+    className?: string
 }
 
-const SpinIcon: React.FC<Props> = ({color, size}) => {
+const SpinIcon: React.FC<Props> = ({color, size, className}) => {
     return (
-        <IonIcon icon='assets/icon/spin-icon.svg' color={color} size={size}/>
+        <IonIcon icon='assets/icon/spin-icon.svg' className={className} color={color} size={size}/>
     )
 }
 

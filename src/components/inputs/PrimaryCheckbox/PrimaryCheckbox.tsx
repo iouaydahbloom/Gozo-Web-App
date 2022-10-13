f63242f1e36c7c66d0e4c9bbcfd8b4344658edb6
Rawad Nasr <rnasr@addbloom.com>
2022-10-13 08:50:59 +0300

    removed the margin of the checkbox

--- src/components/inputs/PrimaryCheckbox/PrimaryCheckbox.tsx
+++ src/components/inputs/PrimaryCheckbox/PrimaryCheckbox.tsx
@@ -4,14 +4,15 @@ import styles from './primaryCheckbox.module.scss';
 
 interface Props {
     value?: boolean,
-    onChange?: (value: boolean) => void
+    onChange?: (value: boolean) => void,
+    className?: string
 }
 
-const PrimaryCheckbox: React.FC<Props> = ({ value, onChange }) => {
+const PrimaryCheckbox: React.FC<Props> = ({ value, onChange, className }) => {
     return (
         <IonCheckbox
             mode='md'
-            className={styles.input}
+            className={`${styles.input} ${className}`}
             value={value}
             checked={value}
             onIonChange={(event) => onChange && onChange(event.detail.checked)} />

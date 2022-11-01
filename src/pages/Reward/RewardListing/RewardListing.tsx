c1debea60795637e31ea56e06fb18841b477ce61
Rawad Nasr <rnasr@addbloom.com>
2022-11-01 13:29:33 +0200

    updated the design of the reward page

--- src/pages/Reward/RewardListing/RewardListing.tsx
+++ src/pages/Reward/RewardListing/RewardListing.tsx
@@ -9,17 +9,16 @@ interface Props {
 
 const RewardListing: React.FC<Props> = ({ rewards }) => {
     return (
-        <IonList className={`${styles.rewardListing}`} inset={true} lines="full">
+        <div className={`${styles.rewardListing}`}>
             {rewards.length !== 0 &&
                 rewards.map((reward, index) => (
                     <RewardListingItem 
                         key={index} 
                         reward={reward} 
-                        // lines={rewards.length - 1 === index ? 'none' : 'full'}
                         />
                 ))
             }
-        </IonList>
+        </div>
     )
 }
 

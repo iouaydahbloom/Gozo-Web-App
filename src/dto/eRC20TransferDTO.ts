26ef67199f3d8cfb1b1bf2eed6a8edbaa277ab12
Rawad Nasr <rnasr@addbloom.com>
2022-11-30 10:00:12 +0200

    added pagination placeholder for token history

---
+++ src/dto/eRC20TransferDTO.ts
@@ -0,0 +1,10 @@
+export interface ERC20TransferDTO {
+    address: string,
+    block_hash: string,
+    block_number: string,
+    block_timestamp: string,
+    from_address: string,
+    to_address: string,
+    transaction_hash: string,
+    value: string
+} 

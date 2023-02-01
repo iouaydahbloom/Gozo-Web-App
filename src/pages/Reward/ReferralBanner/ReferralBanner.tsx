f9f6633cd70d9261ec333416926ea23d2387fe42
rawad <rnasr@addbloom.com>
2023-02-01 11:14:58 +0200

    created the referral banner

--- src/pages/Reward/ReferralBanner/ReferralBanner.tsx
+++ src/pages/Reward/ReferralBanner/ReferralBanner.tsx
@@ -1,17 +1,38 @@
-import PrimaryTypography from '../../../components/typography/PrimaryTypography/PrimaryTypography';
+import PrimarySlider from '../../../components/sliders/PrimarySlider/PrimarySlider';
 import styles from './referralBanner.module.scss';
+
+interface SlideProps {
+    image: string,
+    href: string
+}
 
 const ReferralBanner: React.FC = () => {
 
+    const Slide = ({ image, href }: SlideProps) => {
+        return (
+            <a href={href} className={styles.slide} style={{ backgroundImage: `url(${image})` }}/>
+        )
+    }
 
     return (
-        <div className={styles.container} >
-            <img alt='' src='/assets/image/gozo-element.png' className={styles.startImg}/>
-            <div className={styles.innerContainer}>
-                <PrimaryTypography size='m' customClassName={styles.description}>Send a free spin to a friend today!</PrimaryTypography>
-            </div>
-            <img alt='' src='/assets/image/hands-icon.png' className={styles.endImg}/>
-        </div>
+        <PrimarySlider
+            customStyle={styles.slider}
+            autoPlay
+            slides={[
+                <Slide
+                    image='assets/image/wayaway.png'
+                    href="https://tp.media/click?shmarker=407798&promo_id=7779&source_type=banner&type=click&campaign_id=200&trs=207450" />,
+                <Slide
+                    image='assets/image/economybooking.png'
+                    href="https://tp.media/click?shmarker=407798&promo_id=8155&source_type=banner&type=click&campaign_id=10&trs=207450" />,
+                <Slide
+                    image='assets/image/tripcom.png'
+                    href='https://tp.media/click?shmarker=407798&promo_id=5046&source_type=banner&type=click&campaign_id=121&trs=207450' />,
+                    <Slide
+                    image='assets/image/chearcarrentals.png'
+                    href='https://tp.media/click?shmarker=407798&promo_id=4340&source_type=banner&type=click&campaign_id=117&trs=207450' />
+            ]}
+        />
     )
 }
 
